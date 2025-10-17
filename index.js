60aefe88dd218c8ba7c2b51c974fcdc3d757c73c
Devdave-02 <davidokanla69@gmail.com>
2025-10-17 23:32:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,6 @@ app.use(cors({
   credentials: true
 }));
 
-
-
 // ✅ Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
@@ -33,6 +31,7 @@ app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
 
 
 
